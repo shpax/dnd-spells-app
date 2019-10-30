1ad3b3e2bbca2f89f3096cc7fa03497603fbb3b9
Maxim Piven <shpax1@gmail.com>
2019-10-30 17:18:42 +0200

    removed header reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux'
 import spells from './spells'
 import history from './history'
-import header from './header'
 
-export default combineReducers({ spells, history, header });
+export default combineReducers({ spells, history });
